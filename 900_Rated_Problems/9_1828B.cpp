769a7f8a1c15ab95db8b1e8df595e656247eb019
KunalPusdekar <125309149+KunalPusdekar@users.noreply.github.com>
2025-01-11 18:04:03 +0530

    Create 9_1828B.cpp

---
+++ 900_Rated_Problems/9_1828B.cpp
@@ -0,0 +1,21 @@
+//B. Permutation Swap
+
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int>v(n);
+        for(int i= 0 ; i< n ; i++){
+            cin>>v[i];
+        }
+        int ans=0;
+        for(int i=0 ; i< n ; i++){
+            ans=__gcd(ans,abs(v[i]-(i+1)));
+        }
+        cout<<ans<<endl;
+    }
+}
